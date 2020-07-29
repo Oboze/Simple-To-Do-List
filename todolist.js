db0cec9e6d72252bf536b3c2d4342096880f99fc
Stephen N <stephen.nandi@gmail.com>
2020-07-29 20:45:22 +0300

    Removed JS Code from HTML

---
+++ todolist.js
@@ -0,0 +1,28 @@
+var numberOfListItems=0;
+                var totalItems=document.getElementById('totalItems');
+                totalItems.textContent='You have '+numberOfListItems+ ' activities on your to do list';
+
+            function addActivityToList(){
+                
+
+                var list=document.getElementsByTagName('ul')[0];// set the parent element to add list items
+                var listItem=document.createElement('li');
+                var textValue=document.getElementById('Activity').value;// get the value of the form text input
+                var textItem=document.createTextNode(textValue);
+
+                listItem.appendChild(textItem);
+               list.appendChild(listItem);
+               //list.insertBefore(listItem, list.firstChild);
+               
+               // get all list items from NodeList
+               numberOfListItems=document.querySelectorAll('li').length;
+               console.log(numberOfListItems);
+
+               var totalItems=document.getElementById('totalItems');
+               totalItems.textContent='You have '+numberOfListItems+ ' activities on your to do list';
+
+
+            }
+
+            var addActivity=document.getElementById('addActivity');
+            addActivity.addEventListener('click', function(){addActivityToList();});
